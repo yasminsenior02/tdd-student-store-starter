49b32b49d42b9e2ea1d80755166cb5b26d35141b
yasminsenior02 <yasminsenior02@gmail.com>
2022-06-23 23:59:15 -0700

    attemp on additem to cart

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -3,3 +3,6 @@ import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 
 import "/.ProductGrid.css";
+function ProductGrid({ products, handleAddItemToCart, handleAddItemToCart }) {
+  return <div className="product-grid" />;
+}
